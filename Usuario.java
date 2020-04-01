0e99aa52ed78e3bf11f1bf69bc12a6bd462d9c4b
Bruno <brunoperez1599@gmail.com>
2020-04-01 19:39:08 -0300

    Agregar el archivo usuario

---
+++ Usuario.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuario;
+
+/**
+ *
+ * @author elseg
+ */
+public class Usuario {
+
+    /**
+     * @param args the command line arguments
+     */
+    String Nombre;
+    int Cedula;
+    String Contrasenia;
+    int Edad;
+    int CantidadDeHijos;
+    int Saldo = 0;
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+    
+
