24408e05593f5192d6d43958c34d491d2ecbc24e
Bruno <brunoperez1599@gmail.com>
2020-04-01 19:42:43 -0300

    Se agregaron geters, seters y constructores

--- Usuario.java
+++ Usuario.java
@@ -23,6 +23,62 @@ public class Usuario {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+
+    public Usuario(String Nombre, int Cedula, String Contrasenia, int Edad, int CantidadDeHijos) {
+        this.Nombre = Nombre;
+        this.Cedula = Cedula;
+        this.Contrasenia = Contrasenia;
+        this.Edad = Edad;
+        this.CantidadDeHijos = CantidadDeHijos;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(int Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public String getContrasenia() {
+        return Contrasenia;
+    }
+
+    public void setContrasenia(String Contrasenia) {
+        this.Contrasenia = Contrasenia;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public int getCantidadDeHijos() {
+        return CantidadDeHijos;
+    }
+
+    public void setCantidadDeHijos(int CantidadDeHijos) {
+        this.CantidadDeHijos = CantidadDeHijos;
+    }
+
+    public int getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(int Saldo) {
+        this.Saldo = Saldo;
+    }
     
 }
 
